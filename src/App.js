0b7b8bbc0a610872403b19d0fe9750a589ebce43
kientran175 <en.kientran@gmail.com>
2019-01-08 16:12:37 +0700

    Reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopMenu from './Component/topmenu/TopMenu.js';
+import Header from './Component/header/Header.js';
+import Services from './Component/services/Services.js';
+import Portfolio from './Component/portfolio/Portfolio.js';
+import About from './Component/about/About.js';
+import Team from './Component/team/Team.js';
+import Client from './Component/client/Client.js';
+import Contact from './Component/contact/Contact.js';
+import Footer from './Component/footer/Footer.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Component">
+        <TopMenu/>
+        <Header/>
+        <Services/>
+        <Portfolio/>
+        <About/>
+        <Team/>
+        <Client/>
+        <Contact/>
+        <Footer/>
       </div>
     );
   }
